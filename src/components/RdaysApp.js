51e65abcaee9c785c9d022334e254953b7002679
martin.haesler <martin.haesler@gmail.com>
2015-07-09 19:26:54 +0100

    Remove redundant nav bar from component.

--- src/components/RdaysApp.js
+++ src/components/RdaysApp.js
@@ -10,29 +10,7 @@ require('../styles/main.css');
 var RdaysApp = React.createClass({
   render: function() {
     return (
-            <div class="mdl-layout mdl-js-layout mdl-layout--fixed-drawer mdl-layout--fixed-header">
-  <header className="mdl-layout__header">
-    <div className="mdl-layout__header-row">
-      <span className="mdl-layout-title">Title</span>
-      <div className="mdl-layout-spacer"></div>
-      <nav className="mdl-navigation mdl-layout--large-screen-only">
-        <a className="mdl-navigation__link" href="">Link</a>
-        <a className="mdl-navigation__link" href="">Link</a>
-        <a className="mdl-navigation__link" href="">Link</a>
-        <a className="mdl-navigation__link" href="">Link</a>
-      </nav>
-    </div>
-  </header>
-  <div className="mdl-layout__drawer">
-    <span className="mdl-layout-title">Title</span>
-    <nav className="mdl-navigation">
-      <a className="mdl-navigation__link" href="">Link</a>
-      <a className="mdl-navigation__link" href="">Link</a>
-      <a className="mdl-navigation__link" href="">Link</a>
-      <a className="mdl-navigation__link" href="">Link</a>
-    </nav>
-            </div>
-    </div>
+            <p>Content:</p>
     );
   }
 });
