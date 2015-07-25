e183078d23d39c838a0eb927d783bfd76e40bc1d
mswift42 <martin.haesler@gmail.com>
2015-07-25 13:33:34 +0100

    Add gulpfile.

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var react = require('gulp-react');
+var htmlreplace = require('gulp-html-replace');
+
+var path = {
+  HTML: 'src/index.html',
+  ALL: ['src/js/*.js', 'src/js/**/*.js', 'src/index.html'],
+  JS: ['src/js/*.js', 'src/js/**/*.js'],
+  MINIFIED_OUT: 'build.min.js',
+  DEST_SRC: 'dist/src',
+  DEST_BUILD: 'dist/build',
+  DEST: 'dist'
+};
+
+gulp.task('default', function() {
+  return gulp.src('template.jsx')
+    .pipe(react())
+    .pipe(gulp.dest('dist'));
+});
