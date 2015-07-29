491f2e91b75bfd80cca26360fe5748c5101d60da
mswift42 <martin.haesler@gmail.com>
2015-07-29 18:49:02 +0100

    add editfunctionality.

--- app/components/Task.jsx
+++ app/components/Task.jsx
@@ -1,7 +1,50 @@
 import React from 'react';
 
 export default class Task extends React.Component {
-       render() {
-              return <div>{this.props.value}</div>;
-       }
+    constructor(props) {
+        super(props);
+
+        this.finishEdit = this.finishEdit.bind(this);
+        this.checkEnter = this.checkEnter.bind(this);
+        this.edit = this.edit.bind(this);
+        this.renderEdit = this.renderEdit.bind(this);
+        this.renderValue = this.renderValue.bind(this);
+
+        this.state = {
+            edited: false
+        };
+    }
+    render() {
+        const {value, onEdit, ...props} = this.props;
+        const edited = this.state.edited;
+        return <div>{...props}
+        {edited ? this.renderEdit() : thir.renderValue()}
+        </div>;
+    }
+    renderEdit() {
+        return <input type='text'
+        defaultValue={this.props.value}
+        onBlur={this.finishEdit}
+        onKeyPress={this.checkEnter}/>;
+    }
+    renderValue() {
+        return <div onClick={this.edit}>{this.props.value}</div>;
+    }
+    edit() {
+        this.setState({
+            edited:true
+        });
+    }
+    checkEnter() {
+        if (e.key === 'Enter') {
+            this.finishEdit(e);
+        }
+    }
+    finishEdit() {
+        this.props.onEdit(e.target.value);
+
+        this.setState({
+            edited: false
+        });
+    }
 }
