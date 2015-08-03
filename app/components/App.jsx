010edda52c37b3a36490bf877b74eb861191d4b3
mswift42 <martin.haesler@gmail.com>
2015-08-03 18:43:28 +0100

    Add storage.js

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,46 +1,44 @@
 import React from 'react';
 import Tasks from './Tasks';
+import uuid from 'node-uuid';
+import TaskActions from '../actions/TaskActions';
+import TaskStore from '../stores/TaskStore';
 
 export default class App extends React.Component {
     constructor(props) {
         super(props);
 
-        this.addItem = this.addItem.bind(this);
-        this.itemEdited = this.itemEdited.bind(this);
-        this.state = {
-             tasks: [
-                {summary: "task1"},
-                {summary: "task2"},
-                {summary: "task3"}
-            ]};
-        }
-        render() {
-            const tasks = this.state.tasks;
-
-            return (
-                <div>
-                <button onClick={this.addItem}>+</button>
-                <Tasks items={tasks}
-                 onEdit={this.itemEdited}/>
-                </div>
-            );
-        }
-    itemEdited(i, summary) {
-        let tasks = this.state.tasks;
-        if (summary) {
-            tasks[i].summary = summary;
-        } else {
-            tasks = tasks.slice(0,i).concat(tasks.slice(i+1));
-        }
-        this.setState({
-            tasks:tasks
-        });
+        this.storeChanged = this.storeChanged.bind(this);
+        this.state = TaskStore.getState();
     }
-        addItem() {
-            this.setState({
-                tasks: this.state.tasks.concat([{
-                    summary: "new task"
-                }])
-            });
+    componentDidMount() {
+        TaskStore.listen(this.storeChanged);
+    }
+    componentWillUnmount() {
+        TaskStore.unlisten(this.storeChanged);
+    }
+    storeChanged(state) {
+        this.setState(state);
+    }
+    render() {
+        const tasks = this.state.tasks;
+
+        return (
+            <div>
+                <button onClick={this.addItem}>+</button>
+                <Tasks items={tasks} onEdit={this.itemEdited} />
+            </div>
+        );
+
+    }
+    addItem() {
+        TaskActions.create({id: uuid.v4(), summary: 'New Task'});
+    }
+    ItemEdited(id, summary) {
+        if(summary) {
+            TaskActions.update({id, summary});
+        } else {
+            TaskActions.delete(id);
         }
+    }
 }
